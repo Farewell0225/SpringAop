48ab9cd80e2bdd803ed946f857dde2c31640f8df
yuan_lin@murongtech.com <yuan_lin@murongtech.com>
2019-06-12 18:21:20 +0800

    mybatis c3p0

---
+++ springboot/src/main/java/com/csu/config/DataSourceConfiguration.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2018-2019, XXX有限公司
+ * FileName: DataSourceConfiguration
+ * Author:   yuanlin_csu
+ * Date:     2019/6/12 18:16
+ * Description: 数据源配置
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.csu.config;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈数据源配置〉
+ *
+ * @author yuanlin_csu
+ * @create 2019/6/12
+ * @since 1.0.0
+ */
+public class DataSourceConfiguration {
+
+
+}
